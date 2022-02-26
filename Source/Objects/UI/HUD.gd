c5b71ac8c7b6d7a491bb68fd43cb5bbcdeb2225a
nekotogd <twistedminerdoesutube@gmail.com>
2022-02-26 19:12:04 +0400

    Alternate HUD fic after I broke it

--- Source/Objects/UI/HUD.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _ready():
-	StateTransitionManager.connect("transition_finished", self, "transition_finished")
-
-func transition_finished(is_fps : bool):
-	visible = is_fps
-
