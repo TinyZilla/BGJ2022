6cb83dd683cea07fc355084674cf09830c27998e
TinyZilla <kenlee7539@gmail.com>
2022-02-26 23:40:08 -0800

    sliders

---
+++ Source/Objects/UI/Volume.gd
@@ -0,0 +1,14 @@
+extends HSlider
+
+export var audio_bus_name := "Master"
+
+onready var _bus := AudioServer.get_bus_index(audio_bus_name)
+
+func _ready() -> void:
+	value = db2linear(AudioServer.get_bus_volume_db(_bus))
+	var _t = connect("value_changed", self, "_on_value_changed")
+
+
+func _on_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(_bus, linear2db(value))
+
